aa31987337069648dc24d8d89e151a2a24d951d4
Wesley Reed <reed.wesley.s@gmail.com>
2023-01-14 11:55:05 -0500

    Update badge.js

--- .scripts/badge.js
+++ .scripts/badge.js
@@ -1,11 +1,11 @@
-const git = require(`simple-git/promise`)(__dirname);
+const git = require(`simple-git`)(__dirname);
 const lineReplace = require(`line-replace`);
 
 async function main(){
   const remote = await git.getRemotes(true);
   const originRemote = remote.filter(r => r.name === `origin`)[0].refs.fetch.substring(4).slice(0,-4).replace(`:`,`/`);
 
-  const result = `![Assignment Checks](https://${originRemote}/workflows/Assignment%20Checks/badge.svg)`;
+  const result = `[![GitHub Classroom Workflow](https://${originRemote}/actions/workflows/classroom.yml/badge.svg)](https://${originRemote}/actions/workflows/classroom.yml)`;
 
   lineReplace({
     file: `README.md`,
@@ -23,7 +23,7 @@ async function main(){
       });
     }
   });
-  
+
 }
 
 main();
