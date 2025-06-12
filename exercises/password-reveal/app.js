697fa1a9ba18a89864417dd8cc0925e7e76b55d5
Jack Braun <j.braun3456@gmail.com>
2025-06-12 16:15:43 -0400

    Completed the password reveal excercise and ensure that all tests were successful in cypress

--- exercises/password-reveal/app.js
+++ exercises/password-reveal/app.js
@@ -1,10 +1,26 @@
 // 🤖: create 2 constants with references to the password input and the reveal button from the DOM
 
+const passwordInput = document.getElementById('password');
+const revealButton = document.getElementById('togglePassword');
 
 // 🤖: Create an Event Listener on the password reveal button
 // 🤖: The event listener should toggle the password field attribute between `password` and `text` to change the visibility of the password field content.
 
+revealButton.addEventListener('click', function(e)
+{
 
+const type = passwordInput.getAttribute('type');
+if (type == 'text')
+{
+	passwordInput.setAttribute('type', 'password')
+}
+
+else if (type == 'password')
+{
+	passwordInput.setAttribute('type', 'text')
+}
+
+});
 // Provided for you - prevent form submit
 const form = document.querySelector("form");
 form.addEventListener('submit',function(e){
